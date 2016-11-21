ce6d1f63998180eebc9ba6343e2a4dd01796fd4d
Matt Schuchard <grapheneowns@gmail.com>
2016-11-21 15:43:11 -0500

    improvements to provisioning in pe lab env

--- puppet/vagrant/master.sh
+++ puppet/vagrant/master.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-yum install -y git
-
-/opt/puppetlabs/puppet/bin/gem install --no-rdoc --no-ri puppet-check
-ln -s /opt/puppetlabs/puppet/bin/puppet-check /usr/local/bin/.
-
 echo "service { 'pe-puppetserver': ensure => running }
 
 file { '/etc/puppetlabs/puppet/autosign.conf':
@@ -13,4 +8,17 @@ file { '/etc/puppetlabs/puppet/autosign.conf':
   group   => 'pe-puppet',
   mode    => '0644',
   notify  => Service['pe-puppetserver'],
+}
+
+package { 'git': ensure => latest }
+
+['puppet-check', 'rake'].each |\$bin| {
+  file { \"/usr/local/bin/\${bin}\":
+    ensure => link,
+    target => \"/opt/puppetlabs/puppet/bin/\${bin}\",
+  }
+}
+
+['puppet-check', 'reek', 'rspec-puppet', 'serverspec'].each |\$gem| {
+  exec { \"/opt/puppetlabs/puppet/bin/gem install --no-rdoc --no-ri \${gem}\": }
 }" > /vagrant/.pe_build/post-install/master.pp
