d5f6c9984b1c43bf1c1da33a65af8a6e4a92cc5c
Matt Schuchard <grapheneowns@gmail.com>
2016-11-28 14:15:34 -0500

    adding fixes for multi-platform testing

--- puppet/vagrant/master.sh
+++ puppet/vagrant/master.sh
@@ -1,26 +1,4 @@
 #!/bin/sh
-mkdir -p /vagrant/.pe_build/post-install
-
-echo "service { 'pe-puppetserver': ensure => running }
-
-file { '/etc/puppetlabs/puppet/autosign.conf':
-  ensure  => file,
-  content => \"pe-internal-dashboard\nmaster\n*\",
-  owner   => 'root',
-  group   => 'pe-puppet',
-  mode    => '0644',
-  notify  => Service['pe-puppetserver'],
-}
-
-package { 'git': ensure => latest }
-
-['puppet-check', 'rake', 'rspec-puppet-init', 'serverspec-init'].each |\$bin| {
-  file { \"/usr/local/bin/\${bin}\":
-    ensure => link,
-    target => \"/opt/puppetlabs/puppet/bin/\${bin}\",
-  }
-}
-
-['puppet-check', 'reek', 'rspec-puppet', 'serverspec'].each |\$gem| {
-  exec { \"/opt/puppetlabs/puppet/bin/gem install --no-rdoc --no-ri \${gem}\": }
-}" > /vagrant/.pe_build/post-install/master.pp
+mkdir -p /vagrant/.pe_build/{post-install,answers}
+cp /vagrant/master.pp /vagrant/.pe_build/post-install/master.pp
+cp /vagrant/answers.txt /vagrant/.pe_build/answers/answers.txt
