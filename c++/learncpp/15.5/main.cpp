3f4b5228ecefd0e157c33fb3b32c50a5a82ad935
Matt Schuchard <grapheneowns@gmail.com>
2017-11-15 13:23:54 -0500

    adding c++ exercises

---
+++ c++/learncpp/15.5/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <memory>
+
+class Fraction {
+private:
+	int m_numerator = 0;
+	int m_denominator = 1;
+
+public:
+	Fraction(int numerator, int denominator):
+		m_numerator{numerator}, m_denominator{denominator} {}
+
+	friend std::ostream& operator<<(std::ostream& out, const Fraction &f1) {
+		out << f1.m_numerator << '/' << f1.m_denominator;
+		return out;
+	}
+};
+
+void printFraction(const Fraction* const ptr) {
+	if (ptr)
+		std::cout << *ptr << std::endl;
+}
+
+int main() {
+  // std::unique_ptr<Fraction> ptr(new Fraction(3, 5)); // 11
+  // std::unique_ptr<Fraction> ptr = std::make_unique<Fraction>(3, 5); // 14 without auto
+	auto ptr = std::make_unique<Fraction>(3, 5);
+
+	printFraction(ptr.get());
+
+	return 0;
+}
---
+++ c++/learncpp/15.x/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <memory> // for std::shared_ptr
+
+class Resource
+{
+public:
+	Resource() { std::cout << "Resource acquired\n"; }
+	~Resource() { std::cout << "Resource destroyed\n"; }
+};
+
+class Something {}; // assume Something is a class that can throw an exception
+
+Something doSomething(Something *ptr1, Something *ptr2) {
+  return Something();
+}
+
+int main()
+{
+  auto ptr1 = std::make_shared<Resource>();
+  auto ptr2(ptr1);
+
+  auto somethingPtr = doSomething(std::make_shared<Something>().get(), std::make_shared<Something>().get());
+
+	return 0;
+}
