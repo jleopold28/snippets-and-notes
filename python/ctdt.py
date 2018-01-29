ba48407681004fc4d590d3fd3856141f41b6a3e2
Matt Schuchard <grapheneowns@gmail.com>
2018-01-29 13:00:48 -0500

    adding position full support and fixing weight_phys calc

--- python/ctdt.py
+++ python/ctdt.py
@@ -14,58 +14,69 @@ class CTDTOptimizer(object):
                                 '1E9rwAbRXrU2wjjMe6521z2HxMQnPKdVY8RfVzxzCzyE'
                                 '/export?format=xlsx&id='
                                 '1E9rwAbRXrU2wjjMe6521z2HxMQnPKdVY8RfVzxzCzyE',
-                                sheet_name=None)['FW']
+                                sheet_name=None)
         self.weights = weights
         # create a new dataframe for cleaner output
-        self.new_df = self.__create_dataframe()
+        self.new_df = self.__create_dataframe(positions=weights.keys())
 
     # public
     def display_position_ranked(self, position, sort='Normalized'):
         """display the rankings for a position"""
         # calculate the rank for a position and add to new dataframe
-        self.__calculate_rank(weights=self.weights[position])
+        self.__calculate_rank(position=position)
         # output new dataframe
-        print(self.new_df.sort_values(by=[sort], ascending=False))
+        print(self.new_df[position].sort_values(by=[sort], ascending=False))
 
     # private
-    def __create_dataframe(self):
-        """create a new dataframe to output"""
-        new_df = pd.DataFrame(data={})
-        new_df['Name'] = self.df['Name']
-        new_df['Title'] = self.df['Title']
-        new_df['Class'] = self.df['Class']
+    def __create_dataframe(self, positions):
+        """create a new dataframe"""
+        # init a dict to hold dataframes
+        new_df = {}
+
+        for position in positions:
+            # create a dataframe for a position
+            new_df[position] = pd.DataFrame(data={})
+            # populate the position dataframe with necessary info
+            new_df[position]['Name'] = self.df[position]['Name']
+            new_df[position]['Title'] = self.df[position]['Title']
+            new_df[position]['Class'] = self.df[position]['Class']
 
         # return the modified dataframe
         return new_df
 
-    def __calculate_rank(self, weights):
+    def __calculate_rank(self, position):
         """calculate the rank for a position"""
         # determine new weighted ranking column
-        self.new_df['Weighted'] = self.__calculate_weighted(weights=weights)
+        self.new_df[position]['Weighted'] = self.__calculate_weighted(position=position)
 
         # calculate new normalized column
-        self.new_df['Normalized'] = self.new_df['Weighted'] / self.df['Total']
+        self.new_df[position]['Normalized'] = self.new_df[position]['Weighted'] / self.df[position]['Total']
 
-    def __calculate_weighted(self, weights):
+    def __calculate_weighted(self, position):
         """calculates the weighted ranking for a position"""
-        # determine the phys weight for ranking calculation
-        weight_phys = self.__calculate_weight_phys(weights=weights)
+        # pare down dicts to save time on lookups
+        weights = self.weights[position]
+        df = self.df[position]
 
-        # calculate weighted ranking metric
-        return (weights['dribble'] * self.df['Dribble'] +
-                weights['shot'] * self.df['Shot'] +
-                weights['pass'] * self.df['Pass'] +
-                weights['tackle'] * self.df['Tackle'] +
-                weights['block'] * self.df['Block'] +
-                weights['intercept'] * self.df['Intercept'] +
-                weight_phys * self.df['Physical'])
+        if position == 'GK':
+            # determine the phys weight for ranking calculation
+            # 2 (average among two weights) * 2 (only half contributes)
+            weight_phys = (weights['punch'] + weights['catch']) / 4.0
+            # calculate weighted ranking metric
+            return (weights['punch'] * df['Punch'] +
+                    weights['catch'] * df['Catch'] +
+                    weight_phys * df['Physical'])
+        else:
+            # 6 (average among six weights) * 2 (only half contributes)
+            weight_phys = (weights['dribble'] + weights['shot'] +
+                           weights['pass'] + weights['tackle'] +
+                           weights['block'] + weights['intercept']) / 12.0
+            return (weights['dribble'] * df['Dribble'] +
+                    weights['shot'] * df['Shot'] +
+                    weights['pass'] * df['Pass'] +
+                    weights['tackle'] * df['Tackle'] +
+                    weights['block'] * df['Block'] +
+                    weights['intercept'] * df['Intercept'] +
+                    weight_phys * df['Physical'])
 
-    def __calculate_weight_phys(self, weights):
-        """calculates the weight for physical stats"""
-        # 6 (average among six weights) * 2 (only half contributes) * 3 (three individual stats)
-        return (weights['dribble'] + weights['shot'] +
-                weights['pass'] + weights['tackle'] +
-                weights['block'] + weights['intercept']) / (6 * 2 * 3)
-
-
-# TODO: models and code for other positions (hardcoded to 'FW' in construct for now; goalkeeper is different weight keys), why did combining weight_phys into one weight / 3 and * total phys change the numbers?, tox, init dicts for each position and then pass around position var, privatize attrs; skills, stamina, team skills
+# TODO: tox, privatize attrs; skills, stamina, team skills, gk phys is hypothetically half power to both, half technique to catching, half speed to punching
--- python/ctdt_exec.py
+++ python/ctdt_exec.py
@@ -1,9 +1,12 @@
 import ruamel.yaml as yaml
 from ctdt import CTDTOptimizer
 
+# gather weights as parameter dict from input file
+WEIGHTS = yaml.safe_load(open('params.yaml').read())
 
 # construct class object with parameter dict from input file
-OPT = CTDTOptimizer(yaml.safe_load(open('params.yaml').read()))
+OPT = CTDTOptimizer(WEIGHTS)
 
 # display for each position
-OPT.display_position_ranked(position='FW')
+for position in WEIGHTS.keys():
+    OPT.display_position_ranked(position=position)
