126304e43ec9207e8da43ec3561cea735012dd9d
Matt Schuchard <grapheneowns@gmail.com>
2018-07-18 21:55:08 -0400

    updating testinfra for tomcat app role

--- ansible/workshop/tomcat_hello/molecule/default/tests/test_default.py
+++ ansible/workshop/tomcat_hello/molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -6,9 +7,32 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize('file', [
+    ('/srv/tomcat/catalina/tomcat_hello/app.war'),
+    ('/src/tomcat/catalina/tomcat_hello/hello.config')
+])
+def test_tomcat_file(host, file):
+    tomcat_file = host.file(file)
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert tomcat_file.is_file
+    assert tomcat_file.user == 'tomcat'
+    assert tomcat_file.group == 'tomcat'
+
+
+def test_tomcat_hello(host):
+    tomcat_hello = host.service('tomcat_hello')
+
+    assert tomcat_hello.is_running
+
+
+def test_tomcat(host):
+    tomcat = host.package('tomcat')
+
+    assert tomcat.is_installed
+
+
+def test_tomcat_user(host):
+    tomcat_user = host.user('tomcat')
+
+    # only way to test existence
+    assert tomcat_user.name == 'tomcat'
